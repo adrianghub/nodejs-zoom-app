00311e335c4b790346b6262e019e7dda43b87209
Adrian Zinko <adrian.zinko@allegro.pl>
2020-08-23 17:26:59 +0200

    feat: create room with uuid

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
+const { ESRCH } = require('constants');
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.status(200).send("Hello, World!");
+  res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
 })
 
 server.listen(3000);
